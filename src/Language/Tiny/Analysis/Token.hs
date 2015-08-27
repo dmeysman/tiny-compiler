5086d8043b01eaaab68e84c5741767421de75806
Dylan Meysmans <dmeysman@vub.ac.be>
2015-08-27 21:36:06 +0200

    Do not abuse Show typeclass for tokens

--- src/Language/Tiny/Analysis/Token.hs
+++ src/Language/Tiny/Analysis/Token.hs
@@ -31,37 +31,4 @@ data Token
   | Exclamation
   | Length
   | While
-  deriving Eq
-
-instance Show Token where
-
-  show (Name s) = s
-  show (LiteralInt n) = show n
-  show (LiteralChar c) = show c
-  show Int = "int"
-  show If = "if"
-  show Else = "else"
-  show ExclamationEquals = "!="
-  show Return = "return"
-  show LeftParenthesis = "("
-  show RightParenthesis = ")"
-  show LeftBrace = "{"
-  show RightBrace = "}"
-  show LeftBracket = "["
-  show RightBracket = "]"
-  show Equals = "="
-  show Semicolon = ";"
-  show Comma = ","
-  show Plus = "+"
-  show Minus = "-"
-  show Asterisk = "*"
-  show Slash = "/"
-  show DoubleEquals = "=="
-  show Char = "char"
-  show Write = "write"
-  show Read = "read"
-  show Greater = ">"
-  show Less = "<"
-  show Exclamation = "!"
-  show Length = "length"
-  show While = "while"
+  deriving (Eq, Show)
