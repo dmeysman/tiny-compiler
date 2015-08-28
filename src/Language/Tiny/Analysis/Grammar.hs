ded8c382108ad5cc0f618ffb076e2e38cbe16f6c
Dylan Meysmans <dmeysman@vub.ac.be>
2015-08-28 09:26:53 +0200

    Implement grammar

---
+++ src/Language/Tiny/Analysis/Grammar.hs
@@ -0,0 +1,63 @@
+module Language.Tiny.Analysis.Grammar where
+
+data Program
+  = Program [Declaration]
+  deriving (Eq, Show)
+
+data Declaration
+  = FunctionDeclaration Annotation String [Parameter] Block
+  | VariableDeclaration Variable
+  deriving (Eq, Show)
+
+data Variable
+  = Variable Annotation String
+  deriving (Eq, Show)
+
+data Parameter
+  = Parameter Annotation String
+  deriving (Eq, Show)
+
+data Annotation
+  = IntAnnotation
+  | CharAnnotation
+  | ArrayAnnotation Annotation Expression
+  deriving (Eq, Show)
+
+data Block
+  = Block [Variable] [Statement]
+  deriving (Eq, Show)
+
+data Statement
+  = Conditional Expression Statement
+  | Alternative Expression Statement Statement
+  | Loop Expression Statement
+  | Statements Block
+  | Assignment Lefthand Expression
+  | ReturnStatement Expression
+  | VoidCall String [Expression]
+  | Output Expression
+  | Input Lefthand
+  deriving (Eq, Show)
+
+data Lefthand
+  = ScalarAccess String
+  | ArrayAccess Lefthand Expression
+  deriving (Eq, Show)
+
+data Expression
+  = Lefthand Lefthand
+  | Add Expression Expression
+  | Subtract Expression Expression
+  | Multiply Expression Expression
+  | Divide Expression Expression
+  | Equality Expression Expression
+  | Inequality Expression Expression
+  | GreaterThan Expression Expression
+  | LessThan Expression Expression
+  | Negate Expression
+  | Not Expression
+  | IntValue Int
+  | Call String [Expression]
+  | CharValue Char
+  | ArrayLength Lefthand
+  deriving (Eq, Show)
